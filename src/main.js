7336c4c78d845d7c95bb631ac4ba3d79d5a31ec5
MelLeyva <mleyva0795@gmail.com>
2021-08-19 13:38:57 -0500

    Co-authored-by: Victoria Tejeda <VictoriaTejeda@users.noreply.github.com> Co-authored-by: rossvel-hub <rossvel-hub@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import router from './routes/index.js';
 
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
 myFunction();
