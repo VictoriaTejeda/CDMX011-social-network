180d555403fdd7477d951c11af110b6745945ab6
MelLeyva <mleyva0795@gmail.com>
2021-08-23 01:26:08 -0500

    rotas correctas, con error en signUp al recargar con enter

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -2,23 +2,72 @@
 import { onNavigate } from '../index.js';
 
 export const signUp = () => {
+  document.body.style.background = "url('signUp.png')";
   const divSignUp = document.createElement('div');
+  const formSignUp = document.createElement('form');
+  const br = document.createElement('br');
+  const labelEmail = document.createElement('label');
+  const labelPassword = document.createElement('label');
+  const labelConfirm = document.createElement('label');
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  const confirmPassword = document.createElement('input');
   const buttonSignup = document.createElement('button');
-  buttonSignup.classList.add('btn_signup');
   const buttonReturn = document.createElement('button');
-  buttonReturn.classList.add('btn_return');
   const buttonGoogle = document.createElement('button');
+
+  const emailLabel = document.createTextNode('E-mail');
+  const passwordLabel = document.createTextNode('Password');
+  const confirmLabel = document.createTextNode('Confirm password');
+
+  formSignUp.setAttribute('id', 'signUpForm');
+  inputEmail.setAttribute('type', 'email');
+  inputEmail.setAttribute('id', 'emailSigUp');
+  inputPassword.setAttribute('type', 'password');
+  inputPassword.setAttribute('id', 'idSignUp');
+  confirmPassword.setAttribute('type', 'password');
+  confirmPassword.setAttribute('id', 'idConfirmSignUp');
+  buttonSignup.setAttribute('type', 'submit');
+  buttonSignup.setAttribute('id', 'submitSignUp');
+
+  labelEmail.setAttribute('for', 'emailSigUp');
+  labelEmail.setAttribute('id', 'signUpEmail');
+  labelPassword.setAttribute('for', 'idSignUp');
+  labelPassword.setAttribute('id', 'signUpId');
+  labelConfirm.setAttribute('for', 'idConfirmSignUp');
+  labelConfirm.setAttribute('id', 'signUpConfirmId');
+
+  buttonReturn.classList.add('btn_return');
   buttonGoogle.classList.add('btn_google');
 
   buttonSignup.textContent = 'Sing Up';
   buttonReturn.textContent = 'Home';
-  buttonGoogle.textContent = 'Registrate con Google';
+  buttonGoogle.textContent = 'Sign Up with Google';
 
   buttonReturn.addEventListener('click', () => onNavigate('/'));
 
-  divSignUp.appendChild(buttonSignup);
+  labelEmail.appendChild(emailLabel);
+  labelPassword.appendChild(passwordLabel);
+  labelConfirm.appendChild(confirmLabel);
+
+  formSignUp.appendChild(labelEmail);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(inputEmail);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(labelPassword);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(inputPassword);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(labelConfirm);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(confirmPassword);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(buttonSignup);
+  formSignUp.appendChild(br.cloneNode());
+  formSignUp.appendChild(buttonGoogle);
+
   divSignUp.appendChild(buttonReturn);
-  divSignUp.appendChild(buttonGoogle);
+  divSignUp.appendChild(formSignUp);
 
   return divSignUp;
 };
