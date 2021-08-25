be342d69c8baea3cfc27fa688738ad10113baeeb
MelLeyva <mleyva0795@gmail.com>
2021-08-24 22:23:21 -0500

    unit test Melissa

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,20 @@
-// importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+/*
+ * @jest-environment jsdom
+*/
+import { home } from '../src/components/home';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('Coleccion de test sobre el DOM', () => {
+  let rootdiv = document.createElement('div');
+  rootdiv.setAttribute('id', 'root');
+  document.body.appendChild(rootdiv);
+  home();
+  rootdiv = document.getElementById('root');
+  const divEsperado = document.createElement('div');
+  divEsperado.innerHTML = `
+<div class="section_home"><h1 class="title">Boo!</h1><p class="slogan">Aquí se respira el miedo...cuentame tu historia.</p><div class="container_btn"><button class="btn_sign_up">Regístrate</button><button class="btn_log_in">Inicia sesión</button></div></div>
+`;
+  test('Test sobre render', () => {
+    console.log(rootdiv.outerHTML);
+    expect(rootdiv.outerHTML).toBe(divEsperado.outerHTML);
   });
 });
